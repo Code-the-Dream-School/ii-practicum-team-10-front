7a5d1b01b7ef87dc670e84a4aeb3b5b3d54ceb9a
haibn <haibinyuy@berkeley.edu>
2025-04-28 14:49:39 -0700

    added context for picked class and challenge type

--- src/components/user/learn/css_practice/CssPractice.tsx
+++ src/components/user/learn/css_practice/CssPractice.tsx
@@ -3,9 +3,9 @@ import Card from '../../shared/Card';
 const CssPractice = () => {
   return (
       <div className="flex flex-col justify-center items-center pt-20 lg:flex-row lg:pt-30">
-        <Card colorClass='bg-purple-400' title='Flashcards'>Strengthen your memory with quick recall exercises. Perfect for mastering key concepts, syntax, and definitions using active recall and spaced repetition.</Card>
-        <Card colorClass='bg-purple-400' title='Quiz'>Test your understanding with conceptual and problem-solving questions. Great for identifying knowledge gaps and reinforcing theoretical concepts.</Card>
-        <Card colorClass='bg-purple-400' title='Coding Challenges'>Apply your skills with hands-on exercises. Solve algorithm problems, debug code, and build mini-projects to develop real-world coding experience.</Card>
+        <Card colorClass='bg-purple-400' challengeTitle='Flashcards'>Strengthen your memory with quick recall exercises. Perfect for mastering key concepts, syntax, and definitions using active recall and spaced repetition.</Card>
+        <Card colorClass='bg-purple-400' challengeTitle='Quiz'>Test your understanding with conceptual and problem-solving questions. Great for identifying knowledge gaps and reinforcing theoretical concepts.</Card>
+        <Card colorClass='bg-purple-400' challengeTitle='Coding Challenges'>Apply your skills with hands-on exercises. Solve algorithm problems, debug code, and build mini-projects to develop real-world coding experience.</Card>
       </div>
   );
 };
