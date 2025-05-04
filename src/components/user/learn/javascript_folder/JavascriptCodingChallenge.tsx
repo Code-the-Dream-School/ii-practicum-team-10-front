1b10e1874bcf18594ede445a76938cf3f6888ab8
haibn <haibinyuy@berkeley.edu>
2025-05-03 20:48:58 -0700

    added run tests and submit button

--- src/components/user/learn/javascript_folder/JavascriptCodingChallenge.tsx
+++ src/components/user/learn/javascript_folder/JavascriptCodingChallenge.tsx
@@ -8,17 +8,23 @@ import JavaScriptCodingChallengeOutput from "./JavascriptCodingChallengeOutput";
 const JavaScriptCodingChallenge = () => {
     const { user } = useAuth();
     const token = localStorage.getItem("token");
-    const [questions, setQuestions] = useState<string[]>([]);
-    const [tests, setTests] = useState<any[]>([]);
+    
+    const [question, setQuestion] = useState<string>();
+    const [questionTests, setQuestionTests] = useState<any[]>([]);
+    const [questionId, setQuestionId] = useState<number>();
+    const [functionName, setFunctionName] = useState<string>();
 
     const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [userCode, setUserCode] = useState("// Write your code here");
-    const [result, setResult] = useState<string | null>(null);
+    const [result, setResult] = useState<any[] | null>(null);
+    const [passedAllTests, setPassedAllTests] = useState<boolean>(false);
+    const [showPopup, setShowPopup] = useState(false);
 
     const baseUrl = import.meta.env.VITE_API_JS_CODING_CHALLENGE_URL;
     const jsCodingChallengeUrl = `${baseUrl}JavaScript/codingChallenge`
+    const submitJsCodingChallengeUrl = `${baseUrl}JavaScript/codingChallenge/submit`
 
+    {/* Fetches coding challenge */}
     const fetchJsCodingChallenge = async () => {
         try {
             const response = await fetch(jsCodingChallengeUrl, {
@@ -29,54 +35,85 @@ const JavaScriptCodingChallenge = () => {
                 }
             })
             const data = await response.json();
+            // console.log("Question", data[0].questionText);
+            // console.log("QuestionID", data[0].id);
+            // console.log("QuestionTESTS", data[0].tests);
+            // console.log("Questions QUANTITY", data.length)
 
+            const questionText = data[2].questionText;
 
-            const questionsText: string[] = [];
-            const formattedTests: { input: any[]; expected: any }[][] = [];
-            
-            data.forEach((question: any) => {
-              questionsText.push(question.questionText);
-            
-              const mappedTests = question.tests.map((test: any) => ({
-                input: test.input,
-                expected: test.expectedOutput,
-              }));
-            
-              formattedTests.push(mappedTests);
+            setQuestion(questionText);
+            setQuestionId(data[2].id);
+            setQuestionTests(data[2].tests);
+
+            const match = questionText.match(/`(\w+)`/);
+            const functionName = match ? match[1] : null;
+
+            setFunctionName(functionName);
+
+        } catch(error) {
+            throw console.error();
+        }
+    }
+    
+    {/* Submits the problem if user passes all test cases */}
+    const submitJsCodingChallenge = async () => {
+        if (!passedAllTests) {
+            setShowPopup(true);
+            return; // Exit early
+        }
+
+        try {
+            const response = await fetch(submitJsCodingChallengeUrl, {
+                method: "Post",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                }, 
+                body: JSON.stringify({
+                    "questionId": `${questionId}`
+                }),
             });
-            
-            setQuestions(questionsText);
-            setTests(formattedTests);
 
-        } catch {
+            const data = await response.json();
+
+            console.log("SUBMITTED DATA: ", data);
+        } catch(error) {
             throw console.error();
         }
     }
 
+    {/* Fetches coding challenge problem on load */}
     useEffect(() => {
         fetchJsCodingChallenge();
     }, [])
 
+    {/* Places the cursor inside the editor on load */}
     const onMount = (editor) => {
         editorRef.current = editor;
         editor.focus();
     }
 
+    {/* Returns a set Time Out promise */}
+    const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    {/* Runs user's code on all tests */}
     const runTests = async () => {
         const sourceCode = editorRef.current?.getValue();
         if (!sourceCode) return;
       
-        const currentTests = [{input: [1,5,3], expectedOutput: 5, id: "asd675asd65asd7"}];
-        if (!currentTests || currentTests.length === 0) return;
+        if (!questionTests || questionTests.length === 0) return;
       
-        const testResults: { id: string; passed: boolean; output: string }[] = [];
+        const testResults: { passed: boolean; output: string; expectedOutput: string }[] = [];
+        // console.log("SOURCE CODE", sourceCode);
+        console.log("FUNCTION NAME", functionName);
       
-        for (const test of currentTests) {
+        for (const test of questionTests) {
             console.log("TEST", test)
             const wrappedCode = `
             ${sourceCode}
             
-            console.log(findMax(${JSON.stringify(test.input)}));
+            console.log(${functionName}(${JSON.stringify(test.input)}));
                 `;
       
           try {
@@ -97,77 +134,86 @@ const JavaScriptCodingChallenge = () => {
             });
       
             const data = await response.json();
+            console.log("DATA", data)
             const output = data.run.stdout.trim();
-            console.log("PASSED", output)
+            console.log("OUTPUT", output)
+            const expectedOutput = String(test.expectedOutput);
             const passed = output === String(test.expectedOutput);
       
             testResults.push({
-              id: test.id,
               passed,
               output,
+              expectedOutput
             });
       
           } catch (error) {
             console.error(`Error running test ${test.id}:`, error);
             testResults.push({
-              id: test.id,
               passed: false,
               output: "Execution error",
+              expectedOutput: "Execution error"
             });
           }
+
+          await sleep(200);
         }
       
-        // Format and display result
-        const summary = testResults
-          .map((res, i) =>
-            res.passed
-              ? `Test ${i + 1} passed`
-              : `Test ${i + 1} failed (Output: ${res.output})`
-          )
-          .join("\n");
+        console.log("TEST RESULTS", testResults);
       
-        setResult(summary);
+        setResult(testResults);
+        setPassedAllTests(testResults.every(test => test.passed));
       };
       
     return (
         <div className="flex flex-col justify-center items-center w-full max-w-4xl mx-auto p-4 pt-30">
-            <h2 className="text-xl font-bold mb-4">
-                Question {currentQuestionIndex + 1}: {questions[currentQuestionIndex]}
+            <h2 className="text-xl font-bold w-[850px] mb-4">
+                {question}
             </h2>
 
-            <div className="flex justify-center items-center">
+            <Editor
+                height="400px"
+                width="850px"
+                defaultLanguage="javascript"
+                value={userCode}
+                onChange={(value) => setUserCode(value || "")}
+                onMount={onMount}
+                theme="vs-dark"
+            />
 
-                <Editor
-                    height="400px"
-                    width="400px"
-                    defaultLanguage="javascript"
-                    value={userCode}
-                    onChange={(value) => setUserCode(value || "")}
-                    onMount={onMount}
-                    theme="vs-dark"
-                />
+            {showPopup && (
+                <div className="fixed top-0 left-0 w-full h-full bg-opacity-50 flex justify-center items-center z-50">
+                    <div className="bg-white p-6 rounded-lg shadow-lg max-w-md text-center">
+                    <p className="text-lg font-semibold mb-4">Please pass all the tests before submitting.</p>
+                    <button
+                        onClick={() => setShowPopup(false)}
+                        className="bg-green-500 text-white px-4 py-2 cursor-pointer rounded hover:bg-green-400"
+                    >
+                        OK
+                    </button>
+                    </div>
+                </div>
+            )}
 
-                <JavaScriptCodingChallengeOutput/>
-
-
+            {Array.isArray(result) &&  
+            <div className="flex flex-col justify-center items-center mt-4 bg-gray-100 w-[850px] text-black text-[20px] p-4 rounded">
+                {result.map((res, i) => (
+                    <div
+                        key={res.id}
+                        className={`whitespace-pre-wrap ${res.passed ? "text-green-600" : "text-red-600"}`}
+                    >
+                        {res.passed
+                        ? `Test ${i + 1} passed`
+                        : `Test ${i + 1} failed | Output: ${res.output} | Expected Output: ${res.expectedOutput}`}
+                    </div>
+                    ))
+                }
             </div>
+}
 
-            <div className="mt-4 flex gap-4">
-                <button onClick={runTests} className="bg-green-500 text-white px-4 py-2 rounded">
-                Run Tests
-                </button>
-
-                {currentQuestionIndex < questions.length - 1 && (
-                <button
-                    onClick={() => setCurrentQuestionIndex((prev) => prev + 1)}
-                    className="bg-blue-500 text-white px-4 py-2 rounded"
-                >
-                    Next Question
-                </button>
-                )}
+            <div className="w-[850px] mt-4 flex justify-end items-end gap-3">
+                <button onClick={runTests} className="bg-green-500 text-white px-4 py-2 rounded-xl hover:bg-green-400 hover:cursor-pointer"> Run Tests </button>
+                <button onClick={submitJsCodingChallenge} className="bg-green-500 text-white px-4 py-2 rounded-xl hover:bg-green-400 hover:cursor-pointer">Submit</button>
             </div>
-
-            {result && <p className="mt-4 font-bold">{result}</p>}
         </div>
 
     )
